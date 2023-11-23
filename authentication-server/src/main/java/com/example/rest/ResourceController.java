03f6dd2e28a859047305159ce56bb9fbac429d51
Igor-Gorchakov <Igor_Gorchakov@epam.com>
2023-11-23 23:24:03 +0200

    Fixed code smells

--- authentication-server/src/main/java/com/example/rest/ResourceController.java
+++ authentication-server/src/main/java/com/example/rest/ResourceController.java
@@ -3,13 +3,14 @@ package com.example.rest;
 import com.example.service.VerificationTokenService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @Controller
 @Slf4j
@@ -20,13 +21,13 @@ public class ResourceController {
     @GetMapping("/login")
     public ModelAndView loginPage() {
         log.info("Redirecting a user to the 'Login' page");
-        return new ModelAndView("login-page.html", HttpStatus.OK);
+        return new ModelAndView("login-page.html", OK);
     }
 
     @GetMapping("/verify-sms-code")
     public ModelAndView verifySmsCodePage(Principal principal) {
         log.info("Redirecting a user to the 'Verify sms code' page");
-        return new ModelAndView("verify-sms-code-page.html", HttpStatus.OK);
+        return new ModelAndView("verify-sms-code-page.html", OK);
     }
 
     @PostMapping("/verify-sms-code")
@@ -39,14 +40,14 @@ public class ResourceController {
             return new RedirectView("/home");
         } else {
             log.info("The given SMS code '{}' is NOT VALID for user '{}'", smsCode, username);
-            return new RedirectView("/login");
+            return new RedirectView("/verify-sms-code");
         }
     }
 
     @GetMapping("/home")
     public ModelAndView home(Principal principal) {
-        log.info(principal.getName() + " have been authorized");
-        return new ModelAndView("home-page.html", HttpStatus.OK);
+        log.info("The user '{}' have been authorized", principal.getName());
+        return new ModelAndView("home-page.html", OK);
     }
 
 
