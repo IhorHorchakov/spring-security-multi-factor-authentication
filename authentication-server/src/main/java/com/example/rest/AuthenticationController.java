3a15674d4057743535d0223c4f944261811ab57f
Igor-Gorchakov <Igor_Gorchakov@epam.com>
2023-11-25 17:14:47 +0200

    Renamed LoginController to AuthenticationController

--- authentication-server/src/main/java/com/example/rest/AuthenticationController.java
+++ authentication-server/src/main/java/com/example/rest/AuthenticationController.java
@@ -5,15 +5,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
+import java.util.Map;
+
 import static org.springframework.http.HttpStatus.OK;
 
 @Controller
 @Slf4j
-public class LoginController {
+public class AuthenticationController {
 
     @GetMapping("/login")
     public ModelAndView loginPage() {
         log.info("Redirecting a user to the 'Login' page");
         return new ModelAndView("login-page.html", OK);
+    }
+
+    @GetMapping("/home")
+    public ModelAndView homePage(Principal principal) {
+        String username = principal.getName();
+        log.info("The user '{}' has been authorized", username);
+        return new ModelAndView("home-page.html", Map.of("username", username), OK);
     }
 }
